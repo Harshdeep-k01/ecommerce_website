b932c9c994c2476c9ffc3e8b4bbeffce3d897f56
Harshdeep kaur <72741127+Harshdeep-k01@users.noreply.github.com>
2024-06-27 13:12:33 +0530

    commit

---
+++ src/main/java/com/ecommerce/ecommerce/TestController.java
@@ -0,0 +1,16 @@
+package com.ecommerce.ecommerce;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+   public String test(){
+    return "test";
+   }
+    
+}
